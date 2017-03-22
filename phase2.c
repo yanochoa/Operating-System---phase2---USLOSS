802f487e9de72ced502495ecc1b1b82394695c22
Lucas Kenneth Robbins <lrobbins013@cs.arizona.edu>
2017-03-21 21:11:53 -0700

    Solved merge conflicts with my version and Yan's

--- phase2.c
+++ phase2.c
@@ -5,7 +5,7 @@
    Skeleton file for Phase 2. These routines are very incomplete and are
    intended to give you a starting point. Feel free to use this or not.
 
-  Authors: Yan Ochoa,
+  Authors: Yan Ochoa, Lucas Robbins
    ------------------------------------------------------------------------ */
 
 #include <stdlib.h>
@@ -32,14 +32,23 @@ int paren_pid;
 
 //-------mailbox
 typedef struct Mailbox{
-  P1_Semaphore full;
-  P1_Semaphore mutex;
-  P1_Semaphore empty;
-  int mailboxId;
-  int slots;
-  int slotSize;
+	P1_Semaphore full;
+	P1_Semaphore mutex;
+	P1_Semaphore empty;
+	int mailboxId;
+	int slots;
+	int slotSize;
+	int numMessages;
+	void * messages;
+	
+	//int id;
+	//int slots;
+	//int slotSize;
+	//int openslots;
 
 } Mailbox;
+
+Mailbox mailboxes[P2_MAX_MBOX]; 
 
 //globals
 int global_parentID;
@@ -85,6 +94,12 @@ int P2_Startup(void *arg){
      * Create the other device drivers.
      */
     // ...
+	
+	// Initialize mailbox table
+	int i;
+	for (i=0; i < P2_MAX_MBOX; i++){
+		mailboxes[i] = NULL;
+	}
 
     /* 
      * Create initial user-level process. You'll need to check error codes, etc. P2_Spawn
@@ -168,40 +183,43 @@ done:
 }
 
 
-
-
 //--------------------------------------------------------
 
 //--------------------------------------------------------
 int P2_Spawn(char *name, int (*func)(void *arg), void *arg, int stackSize, int priority) {
-// USLOSS_Console("Entered P2_Spawn");
-//check for a valid priority
-  if((priority < 0) || (priority > 4)){
-	return -3;
-  }
- //check the stacksize
- if(stackSize < USLOSS_MIN_STACK){
-  return -2;
-  }
-
-
-
-  //check the mode
-  if((USLOSS_PSR_CURRENT_MODE & USLOSS_PsrGet())==0){
-	  USLOSS_Console("Not in kernel mode [p2_Spawn]");
-	 //goto done;
+	// USLOSS_Console("Entered P2_Spawn");
+	//check for a valid priority
+	if((priority < 0) || (priority > 4)){
+		return -3;
 	}
-//USLOSS_Console("about to run\n");
+	//check the stacksize
+	if(stackSize < USLOSS_MIN_STACK){
+		return -2;
+	}
+		//check the mode
+	if((USLOSS_PSR_CURRENT_MODE & USLOSS_PsrGet())==0){
+		USLOSS_Console("Not in kernel mode [p2_Spawn]");
+		//goto done;
+	}
+	//USLOSS_Console("about to run\n");
 
-  //P1_P(running);
-  parentPID = P1_GetPID();
-  int newPID = P1_Fork(name,launch_p,arg,stackSize,priority, 0);
-  //P1_V(running);
-//USLOSS_Console("abt to return from P2_Spawn\n");
+	//P1_P(running);
+	parentPID = P1_GetPID();
+	int newPID = P1_Fork(name,launch_p,arg,stackSize,priority, 0);
+	//P1_V(running);
+	//USLOSS_Console("abt to return from P2_Spawn\n");
     return newPID;
-
-
 }
+
+//------------------------------------------------------
+//similar to P1_Join but for user level proesses.
+// returns if a child was terminated. return the PID of the child process that exited .
+int     
+P2_Wait(int *status) 
+{
+    return -1;
+}
+
 //-------------------------------------------------------
 //similar to P1_Quit except for user level processes
 // returns status to P2_Wait called by its parent.
@@ -210,30 +228,19 @@ void P2_Terminate(int status){
   //P2_Wait(status);
 }
 
-
-
 //--------------------------------------------------------
 //helper function launches func as user level
 int launch_p(void *arg){
-//set usermode
+	//set usermode
 
 
-//use a for something
-int a = USLOSS_PsrSet(USLOSS_PsrGet() & ~USLOSS_PSR_CURRENT_MODE);
-  helper_PCB incoming_process;
+	//use a for something
+	int a = USLOSS_PsrSet(USLOSS_PsrGet() & ~USLOSS_PSR_CURRENT_MODE);
+	helper_PCB incoming_process;
 
-  incoming_process.paren_pid = global_parentID;
+		incoming_process.paren_pid = global_parentID;
 
-return a;  //change ret value later
-}
-
-//------------------------------------------------------
-//similar to P1_Join but for user level proesses.
-// returns if a child was terminated. return the PID of the child process that exited .
-
-int P2_Wait(int *status) 
-{
-    return -1;
+	return a;  //change ret value later
 }
 
 
@@ -249,3 +256,242 @@ int P2_Sleep(int seconds){
 
 }
 
+int
+P2_MboxCreate(int slots, int size)
+{
+	int i;
+	for (i = 0; i < P2_MAX_MBOX; i++){
+		if (mailboxes[i] == NULL) {
+			break;
+		}
+	}
+	
+	if (i == P2_MAX_MBOX) {
+		return -1
+	}
+	
+	Mailbox * newbox = (Mailbox *)malloc(sizeof(Mailbox));
+	P1_SemCreate("full", size, &newbox->full);
+	P1_SemCreate("mutex", 1, &newbox->mutex);
+	P1_SemCreate("empty", size, &newbox->empty);
+	newbox->slots = slots;
+	newbox->slotSize = size;
+	newbox->mailboxId = i;
+	newbox->numMessages = 0;
+	newbox->messages = (void *) malloc(sizeof(char) * size * slots);
+	
+	mailboxes[i] = newbox;
+	
+	return i;
+}
+
+int 
+P2_MboxRelease(int mbox)
+{
+	if (mbox < 0 || mbox >= P2_MAX_MBOX || mailboxes[mbox]==NULL) {
+		return -1;
+	}
+	
+	P1_P(mailboxes[mbox]->mutex);
+	mailboxes[mbox] = NULL;
+	
+	return 0;
+}
+
+int
+P2_MboxSend(int mbox, void *msg, int *size) 
+{
+	if (mbox < 0 || mbox >= P2_MAX_MBOX || mailboxes[mbox]==NULL) {
+		return -1;
+	}
+	
+	if (mailboxes[mbox] == NULL) {
+		return -2;
+	}
+	
+	Mailbox * ptr = mailboxes[mailbox];
+	
+	if (ptr->slotSize < *size) {
+		return -1;
+	}
+	
+	P1_P(ptr->full);
+	P1_P(ptr->mutex);
+	
+	ptr->message[ptr->numMessages * ptr->slotSize] = msg;
+	ptr->numMessages++;
+	
+	P1_V(ptr->mutex);
+	P1_V(ptr->empty);
+	
+	return 0;
+}
+
+int
+P2_MboxCondSend(int mbox, void *msg, int *size)
+{
+	if (mbox < 0 || mbox >= P2_MAX_MBOX || mailboxes[mbox]==NULL) {
+		return -1;
+	}
+	
+	if (mailboxes[mbox] == NULL) {
+		return -2;
+	}
+	
+	Mailbox * ptr = mailboxes[mailbox];
+	
+	if (ptr->slotSize < *size) {
+		return -1;
+	}
+	
+	if(ptr->numMessages == ptr->slots){
+		return 1;
+	}
+	
+	P1_P(ptr->full);
+	P1_P(ptr->mutex);
+	
+	ptr->message[ptr->numMessages * ptr->slotSize] = msg;
+	ptr->numMessages++;
+	
+	P1_V(ptr->mutex);
+	P1_V(ptr->empty);
+	
+	return 0;
+}
+
+
+int
+P2_MboxReceive(int mbox, void *msg, int *size)
+{
+	if (mbox < 0 || mbox >= P2_MAX_MBOX || mailboxes[mbox]==NULL) {
+		return -1;
+	}	
+	
+	if (mailboxes[mbox] == NULL) {
+		return -2;
+	}
+	
+	Mailbox * ptr = mailboxes[mailbox];
+	
+	char msg_buff[ptr->slots * ptr->slotSize] = {'\0'};
+	
+	P1_P(ptr->empty);
+	P1_P(ptr->mutex);
+	
+	msg_buff = ptr->message[ptr->numMessages * ptr->slotSize];
+	char reset_buff[ptr->slots * ptr->slotSize] = {'\0'};
+	ptr->message[ptr->numMessages * ptr->slotSize] = reset_buff;
+	ptr->numMessages--;
+	
+	P1_V(ptr->full);
+	p1_V(ptr->mutex);
+	
+	msg = (void *) msg_buff;
+
+	int i;
+	for (i = 0; i < ptr->slotSize && msg_buff[i] != '\0'; i++) {
+	}
+	
+	*size = i;
+	
+	return 0;
+}
+
+int
+P2_MboxCondReceive(int mbox, void *msg, int *size)
+{
+	if (mbox < 0 || mbox >= P2_MAX_MBOX || mailboxes[mbox]==NULL) {
+		return -1;
+	}
+	
+	if (mailboxes[mbox] == NULL) {
+		return -2;
+	}
+	
+	Mailbox * ptr = mailboxes[mailbox];
+	
+	char msg_buff[ptr->slots * ptr->slotSize] = {'\0'};
+	
+	if (numMessages == 0){
+		return 1;
+	}
+	
+	P1_P(ptr->empty);
+	P1_P(ptr->mutex);
+	
+	msg_buff = ptr->message[ptr->numMessages * ptr->slotSize];
+	char reset_buff[ptr->slots * ptr->slotSize] = {'\0'};
+	ptr->message[ptr->numMessages * ptr->slotSize] = reset_buff;
+	ptr->numMessages--;
+	
+	P1_V(ptr->full);
+	p1_V(ptr->mutex);
+	
+	msg = (void *) msg_buff;
+
+	int i;
+	for (i = 0; i < ptr->slotSize && msg_buff[i] != '\0'; i++) {
+	}
+	
+	*size = i;
+	
+	return 0;
+}
+
+
+int
+P2_TermRead(int unit, int size, char *buffer) 
+{
+	int status = USLOSS_DEV_READY;
+	char terminal[2];
+	
+	if (size < 0 || size > P2_MAX_LINE) {
+		return -1;
+	}
+	
+		// Wait for a character to be transmitted to the terminal
+	while (status == USLOSS_DEV_READY) {
+		USLOSS_DeviceInput(USLOSS_TERM_DEV, unit, &terminal);
+		status = USLOSS_TERM_STAT_RECV(terminal);
+	}
+	
+	if (status == USLOSS_DEV_ERROR) {
+		return -1;
+	}
+	
+	char curr_char = terminal[0];
+	buffer[0] = curr_char;
+	
+	i = 1;
+	while (status == USLOSS_DEV_BUSY && i < size) {
+		USLOSS_DeviceInput(USLOSS_TERM_DEV, unit, terminal);
+		status = USLOSS_TERM_STAT_RECV(terminal);
+		char curr_char = terminal[0];
+		buffer[i] = curr_char;
+	}
+	
+	size = i;
+	return 0;
+}
+
+void
+P2_TermWrite(int unit, int size, char *text) 
+{
+	int status = USLOSS_DEV_READY;
+	char terminal[2];
+	
+	if (size < 0 || size > P2_MAX_LINE) {
+		return -1;
+	}
+	
+	int i;
+	for (i = 0; i < size; i++) {
+		terminal[0] = text[i];
+		terminal[1] = 0x1;
+		USLOSS_DeviceOutput(USLOSS_TERM_DEV, unit, &terminal);
+	}
+	
+	size = i;
+	return 0;
+}
